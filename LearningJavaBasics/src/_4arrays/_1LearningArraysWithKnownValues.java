7940b26fe647ec1a1d9ec4ecdd60e4175502db2c
TestingTequila <testing.stpl1@gmail.com>
2022-06-02 08:17:37 +0530

    Arrays With known and unknown values

---
+++ LearningJavaBasics/src/_4arrays/_1LearningArraysWithKnownValues.java
@@ -0,0 +1,43 @@
+package _4arrays;
+
+public class _1LearningArraysWithKnownValues {
+
+	public static void main(String[] args) {
+		// WHAT ARE ARRAYS? Arrays are also like containers/Jars with multiple
+		// compartments/section
+		// that can hold multiple values.
+
+		// 12, 14, 15, 17, 98, 11, 22, 33, 87, 99
+
+		// dataType[] arrayName = {value1, value2, value3, value4......valueN}
+
+		// WAP to print all the numbers mentioned above
+		
+		int[] numbers = { 12, 14, 15, 17, 98, 11, 22, 33, 87, 99 };
+		System.out.println("=============For Loop Result======================================");
+		
+		for(int x=0; x<numbers.length; x=x+3)
+		{
+			System.out.println(numbers[x]);// 12, 15, 98
+		}
+		
+		System.out.println("=============Advanced For Loop Result======================================");
+		
+		for(int num : numbers)
+		{
+			System.out.println(num);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ LearningJavaBasics/src/_4arrays/_2LearningArraysWithUnknownValues.java
@@ -0,0 +1,29 @@
+package _4arrays;
+
+public class _2LearningArraysWithUnknownValues {
+
+	public static void main(String[] args) 
+	{
+		// dataType[] arrayName = new dataType[numberOfItemsYouWantToHold];
+		
+		int[] numbers = new int[7];
+		numbers[3]=12;
+		numbers[5]=100;
+		numbers[0]=45;
+		numbers[1]=67;
+		numbers[6]=55;
+		numbers[2]=76;
+		numbers[4]=44;
+	
+		
+		for(int num: numbers)
+		{
+			System.out.println(num);
+		}
+		
+		
+		
+		
+		
+	}
+}
