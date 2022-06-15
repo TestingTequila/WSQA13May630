63429504c6fc3e80cdfc82918a6b947e6c028e41
Ashish Mishra <ashish.mishra.sse@gmail.com>
2022-06-15 07:20:31 +0530

    Constructors - Implicite and Explicite

---
+++ LearningJavaBasics/src/_16constructors/ExecutingConstructors.java
@@ -0,0 +1,18 @@
+package _16constructors;
+
+public class ExecutingConstructors {
+
+	public static void main(String[] args) {
+
+		_1LearningConstructors lc = new _1LearningConstructors(12, 12.34, "Jason", true);
+		
+		System.out.println(lc.a);
+		System.out.println(lc.b);
+		System.out.println(lc.c);
+		System.out.println(lc.d);
+		System.out.println(lc.adddition());
+		System.out.println(lc.subtraction());
+
+	}
+
+}
---
+++ LearningJavaBasics/src/_16constructors/_1LearningConstructors.java
@@ -0,0 +1,30 @@
+package _16constructors;
+
+public class _1LearningConstructors {
+
+	public _1LearningConstructors(int a, double b, String c, boolean d)
+	{
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		this.d=d;
+	}
+	
+	int a;
+	double b;
+	String c;
+	boolean d;
+	
+	public double adddition()
+	{
+		return (a+b);
+		
+	}
+	
+	public double subtraction()
+	{
+		return (a-b);
+		
+	}
+	
+}
