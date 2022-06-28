10ced9373bb4f2aa2703638c993416577c762203
Ashish Mishra <ashish.mishra.sse@gmail.com>
2022-06-28 08:12:30 +0530

    Updated

--- LearningJavaBasics/src/_23POMWithPFMAndTestNG/PreRequisites.java
+++ LearningJavaBasics/src/_23POMWithPFMAndTestNG/PreRequisites.java
@@ -3,14 +3,14 @@ package _23POMWithPFMAndTestNG;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class PreRequisites extends ApplicationData{
 
-	String browserType = "Chrome";
 	WebDriver driver;
 
-	@BeforeMethod
-	public void OpenApplication() {
+	@BeforeMethod @Parameters("browserType")
+	public void OpenApplication(String browserType) {
 		// 1. Open the browser
 		driver = _19utilities._1CommonActions.launchBrowser(browserType);
 		driver.manage().window().maximize();
